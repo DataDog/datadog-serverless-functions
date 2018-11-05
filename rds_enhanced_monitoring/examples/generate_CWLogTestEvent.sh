8d09241473c64d89e0234ebfd28545dee271df06
Marc Ian Bucad <ian.bucad@gmail.com>
2018-11-05 16:05:44 +1100

    clean up

--- rds_enhanced_monitoring/examples/generate_CWLogTestEvent.sh
+++ rds_enhanced_monitoring/examples/generate_CWLogTestEvent.sh
@@ -4,16 +4,34 @@ SCRIPTPATH=$(cd `dirname $0` && pwd)
 
 ts=$(date +%s)000
 tmp_CWLogEvent='''
-{ "awslogs": {
-	"data":"__b64data__"
-	}
+{
+  "awslogs": {
+    "data":"__b64data__"
+  }
+}
+'''
+
+tmp_data='''
+{
+  "logStream": "db-ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "messageType": "DATA_MESSAGE",
+  "logEvents": [
+    {
+      "timestamp": __timestamp__,
+      "message": "__message__"
+    }
+  ],
+  "owner": "123456789000",
+  "subscriptionFilters": [
+    "dd-rdsenhanced-filter"
+  ],
+  "logGroup": "RDSOSMetrics"
 }
 '''
 
 tmp_message=$(sed -e 's/"/\\"/g' $SCRIPTPATH/tmp_message.json)
-tmp_CWLog=$(cat $SCRIPTPATH/tmp_CWLog.json)
 
-tmp_CWLogMessage=${tmp_CWLog/__message__/$tmp_message}
-b64_CWLogMessage=$(echo ${tmp_CWLogMessage/__timestamp__/$ts} | gzip | base64 -w0)
+tmp_CWLogRaw=${tmp_data/__message__/$tmp_message}
+tmp_CWLogB64=$(echo ${tmp_CWLogRaw/__timestamp__/$ts} | gzip | base64 -w0)
 
-printf "${tmp_CWLogEvent/__b64data__/$b64_CWLogMessage}"
+printf "${tmp_CWLogEvent/__b64data__/$tmp_CWLogB64}"
