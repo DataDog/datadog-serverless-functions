595ef4728613559c06d9d1a9f418e52ac725ba22
ericmustin <mustin.eric@gmail.com>
2019-08-12 14:18:53 +0200

    remove tests for now

--- aws/logs_monitoring/lambda_function.py
+++ aws/logs_monitoring/lambda_function.py
@@ -104,39 +104,37 @@ exclude_regex = compileRegex("EXCLUDE_AT_MATCH", EXCLUDE_AT_MATCH)
 
 # DD_API_KEY: Datadog API Key
 DD_API_KEY = "<your_api_key>"
+if "DD_KMS_API_KEY" in os.environ:
+    ENCRYPTED = os.environ["DD_KMS_API_KEY"]
+    DD_API_KEY = boto3.client("kms").decrypt(
+        CiphertextBlob=base64.b64decode(ENCRYPTED)
+    )["Plaintext"]
+elif "DD_API_KEY" in os.environ:
+    DD_API_KEY = os.environ["DD_API_KEY"]
 
-def validation():
-    if "DD_KMS_API_KEY" in os.environ:
-        ENCRYPTED = os.environ["DD_KMS_API_KEY"]
-        DD_API_KEY = boto3.client("kms").decrypt(
-            CiphertextBlob=base64.b64decode(ENCRYPTED)
-        )["Plaintext"]
-    elif "DD_API_KEY" in os.environ:
-        DD_API_KEY = os.environ["DD_API_KEY"]
+# Strip any trailing and leading whitespace from the API key
+DD_API_KEY = DD_API_KEY.strip()
 
-    # Strip any trailing and leading whitespace from the API key
-    DD_API_KEY = DD_API_KEY.strip()
-
-    # DD_API_KEY must be set
-    if DD_API_KEY == "<your_api_key>" or DD_API_KEY == "":
-        raise Exception(
-            "You must configure your Datadog API key using "
-            "DD_KMS_API_KEY or DD_API_KEY"
-        )
-    # Check if the API key is the correct number of characters
-    if len(DD_API_KEY) != 32:
-        raise Exception(
-            "The API key is not the expected length. "
-            "Please confirm that your API key is correct"
-        )
-
-    # Validate the API key
-    validation_res = requests.get(
-        "https://api.{}/api/v1/validate?api_key={}".format(DD_SITE, DD_API_KEY)
+# DD_API_KEY must be set
+if DD_API_KEY == "<your_api_key>" or DD_API_KEY == "":
+    raise Exception(
+        "You must configure your Datadog API key using "
+        "DD_KMS_API_KEY or DD_API_KEY"
+    )
+# Check if the API key is the correct number of characters
+if len(DD_API_KEY) != 32:
+    raise Exception(
+        "The API key is not the expected length. "
+        "Please confirm that your API key is correct"
     )
 
-    if not validation_res.ok:
-        raise Exception("The API key is not valid.")
+# Validate the API key
+validation_res = requests.get(
+    "https://api.{}/api/v1/validate?api_key={}".format(DD_SITE, DD_API_KEY)
+)
+
+if not validation_res.ok:
+    raise Exception("The API key is not valid.")
 
 def create_multiline_start_regex(start_pattern):
     return re.compile(
@@ -408,7 +406,6 @@ class DatadogScrubber(object):
 
 def datadog_forwarder(event, context):
     """The actual lambda function entry point"""
-    validation()
     events = parse(event, context)
     metrics, logs = split(events)
     if DD_FORWARD_LOG:
