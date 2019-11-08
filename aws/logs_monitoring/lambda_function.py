24bb0a53078f5bc31fbf68ae709ea4add90d7cd4
Gaetan Deputier <48209202+gaetan-deputier@users.noreply.github.com>
2019-11-08 15:47:24 +0100

    Update aws/logs_monitoring/lambda_function.py
    Co-Authored-By: Stephen Firrincieli <stephen.firrincieli@datadoghq.com>

--- aws/logs_monitoring/lambda_function.py
+++ aws/logs_monitoring/lambda_function.py
@@ -255,7 +255,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "2.3.1"
+DD_FORWARDER_VERSION = "2.3.2"
 
 class RetriableException(Exception):
     pass
