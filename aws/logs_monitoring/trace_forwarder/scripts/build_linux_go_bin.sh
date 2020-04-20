bb203ef5d0716e00f62333920da77fccbbf3efb7
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2020-04-20 13:36:49 -0400

    Fix bad substitution error

--- aws/logs_monitoring/trace_forwarder/scripts/build_linux_go_bin.sh
+++ aws/logs_monitoring/trace_forwarder/scripts/build_linux_go_bin.sh
@@ -10,7 +10,8 @@ set -e
 
 # Change to the parent of the directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
+cd "${DIR}/.."
+
 
 rm -rf ./bin
 
