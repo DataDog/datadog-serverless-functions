b06f8cd956033ec3edf1e9b65190a905d34577a0
Jordan Storms <jordan.storms@datadoghq.com>
2020-06-08 11:45:43 -0400

    remove enhanced lambda redundancies

--- aws/logs_monitoring/enhanced_lambda_metrics.py
+++ aws/logs_monitoring/enhanced_lambda_metrics.py
@@ -451,24 +451,11 @@ def parse_metrics_from_report_log(report_log_line):
     else:
         tags.append("cold_start:false")
 
-    # if cold_start:
-    if regex_match.group(INIT_DURATION_METRIC_NAME):
-        metric_point_value = float(regex_match.group(INIT_DURATION_METRIC_NAME))
-        # Multiply by 1/1000 to convert ms to seconds
-        metric_point_value *= METRIC_ADJUSTMENT_FACTORS[INIT_DURATION_METRIC_NAME]
-
-        initial_duration = DatadogMetricPoint(
-            "{}.{}".format(
-                ENHANCED_METRICS_NAMESPACE_PREFIX, INIT_DURATION_METRIC_NAME
-            ),
-            metric_point_value,
-        )
-
-        initial_duration.add_tags(tags)
-
-        metrics.append(initial_duration)
-
     for metric_name in METRICS_TO_PARSE_FROM_REPORT:
+        # check if init duration is present
+        if not regex_match.gropu(metric_name):
+            continue
+
         metric_point_value = float(regex_match.group(metric_name))
         # Multiply the duration metrics by 1/1000 to convert ms to seconds
         if metric_name in METRIC_ADJUSTMENT_FACTORS:
