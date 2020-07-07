129dcd1ead4f1497af769538c1588ee12faba67d
Nick Hinsch <nick.hinsch@datadoghq.com>
2020-07-07 10:11:50 -0400

    Mock datadog

--- aws/logs_monitoring/tests/test_lambda_function.py
+++ aws/logs_monitoring/tests/test_lambda_function.py
@@ -4,6 +4,7 @@ import sys
 
 sys.modules["datadog_lambda.wrapper"] = MagicMock()
 sys.modules["datadog_lambda.metric"] = MagicMock()
+sys.modules["datadog"] = MagicMock()
 
 from lambda_function import batch_trace_payloads
 
