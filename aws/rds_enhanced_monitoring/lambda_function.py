b906cc32f5037fcdcf1d407f7fad4e68863f3c62
Jonathan VanBriesen <jon.vanbriesen@datadoghq.com>
2020-07-26 20:20:44 -0400

    adding decryption fix

--- aws/rds_enhanced_monitoring/lambda_function.py
+++ aws/rds_enhanced_monitoring/lambda_function.py
@@ -20,7 +20,10 @@ DD_SITE = os.getenv("DD_SITE", default="datadoghq.com")
 # retrieve datadog options from KMS
 KMS_ENCRYPTED_KEYS = os.environ['kmsEncryptedKeys']
 kms = boto3.client('kms')
-datadog_keys = json.loads(kms.decrypt(CiphertextBlob=base64.b64decode(KMS_ENCRYPTED_KEYS))['Plaintext'])
+datadog_keys = json.loads(kms.decrypt(
+    CiphertextBlob=b64decode(KMS_ENCRYPTED_KEYS),
+    EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+)['Plaintext'])
 
 print('INFO Lambda function initialized, ready to send metrics')
 
