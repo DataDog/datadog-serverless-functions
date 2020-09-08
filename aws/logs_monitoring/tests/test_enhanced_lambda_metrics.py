b32c5d46bb6cb77ac5a47c29b2d3a2fa459b5308
hghotra <ghotra.harvinder@gmail.com>
2020-09-08 09:26:39 -0400

    Case when s3 cache is expired

--- aws/logs_monitoring/tests/test_enhanced_lambda_metrics.py
+++ aws/logs_monitoring/tests/test_enhanced_lambda_metrics.py
@@ -309,9 +309,9 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
-        mock_build_cache.return_value = (
+        mock_get_s3_cache.return_value = (
             {
                 "arn:aws:lambda:us-east-1:172597598159:function:post-coupon-prod-us": [
                     "team:metrics",
@@ -322,7 +322,6 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
             },
             time(),
         )
-        mock_forward_metrics.return_value = None
         tags_cache = LambdaTagsCache()
 
         logs_input = {
@@ -408,9 +407,9 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics_with_tags(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
-        mock_build_cache.return_value = (
+        mock_get_s3_cache.return_value = (
             {
                 "arn:aws:lambda:us-east-1:172597598159:function:post-coupon-prod-us": [
                     "team:metrics",
@@ -521,9 +520,9 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics_once_with_missing_arn(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
-        mock_build_cache.return_value = ({}, time())
+        mock_get_s3_cache.return_value = ({}, time())
         tags_cache = LambdaTagsCache()
 
         logs_input = {
@@ -546,20 +545,20 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
         os.environ["DD_FETCH_LAMBDA_TAGS"] = "True"
 
         generated_metrics = generate_enhanced_lambda_metrics(logs_input, tags_cache)
-        mock_build_cache.assert_called_once()
-        mock_build_cache.reset_mock()
+        mock_get_s3_cache.assert_called_once()
+        mock_get_s3_cache.reset_mock()
 
         generated_metrics = generate_enhanced_lambda_metrics(logs_input, tags_cache)
-        mock_build_cache.assert_not_called()
+        mock_get_s3_cache.assert_not_called()
 
         del os.environ["DD_FETCH_LAMBDA_TAGS"]
 
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics_refresh_on_new_arn(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
-        mock_build_cache.return_value = (
+        mock_get_s3_cache.return_value = (
             {
                 "arn:aws:lambda:us-east-1:172597598159:function:post-coupon-prod-us": [
                     "team:metrics",
@@ -592,18 +591,70 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
         os.environ["DD_FETCH_LAMBDA_TAGS"] = "True"
 
         generated_metrics = generate_enhanced_lambda_metrics(logs_input, tags_cache)
+        mock_get_s3_cache.assert_called_once()
+        mock_get_s3_cache.reset_mock()
+
+        del os.environ["DD_FETCH_LAMBDA_TAGS"]
+
+    @patch("enhanced_lambda_metrics.write_cache_to_s3")
+    @patch("enhanced_lambda_metrics.build_tags_by_arn_cache")
+    @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
+    @patch("enhanced_lambda_metrics.get_cache_from_s3")
+    def test_generate_enhanced_lambda_metrics_refresh_s3_cache(
+        self,
+        mock_get_s3_cache,
+        mock_forward_metrics,
+        mock_build_cache,
+        mock_write_cache,
+    ):
+        mock_get_s3_cache.return_value = (
+            {},
+            1000,
+        )
+        mock_build_cache.return_value = {
+            "arn:aws:lambda:us-east-1:172597598159:function:post-coupon-prod-us": [
+                "team:metrics",
+                "monitor:datadog",
+                "env:prod",
+                "creator:swf",
+            ]
+        }
+        tags_cache = LambdaTagsCache()
+
+        logs_input = {
+            "message": "REPORT RequestId: fe1467d6-1458-4e20-8e40-9aaa4be7a0f4\tDuration: 3470.65 ms\tBilled Duration: 3500 ms\tMemory Size: 128 MB\tMax Memory Used: 89 MB\t\nXRAY TraceId: 1-5d8bba5a-dc2932496a65bab91d2d42d4\tSegmentId: 5ff79d2a06b82ad6\tSampled: true\t\n",
+            "aws": {
+                "awslogs": {
+                    "logGroup": "/aws/lambda/post-coupon-prod-us",
+                    "logStream": "2019/09/25/[$LATEST]d6c10ebbd9cb48dba94a7d9b874b49bb",
+                    "owner": "172597598159",
+                },
+                "function_version": "$LATEST",
+                "invoked_function_arn": "arn:aws:lambda:us-east-1:172597598159:function:collect_logs_datadog_demo",
+            },
+            "lambda": {
+                "arn": "arn:aws:lambda:us-east-1:172597598159:function:post-coupon-prod-us"
+            },
+            "timestamp": 10000,
+        }
+
+        os.environ["DD_FETCH_LAMBDA_TAGS"] = "True"
+
+        generated_metrics = generate_enhanced_lambda_metrics(logs_input, tags_cache)
+        mock_get_s3_cache.assert_called_once()
         mock_build_cache.assert_called_once()
-        mock_build_cache.reset_mock()
+        mock_write_cache.assert_called_once()
+        mock_get_s3_cache.reset_mock()
 
         del os.environ["DD_FETCH_LAMBDA_TAGS"]
 
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics_timeout(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
 
-        mock_build_cache.return_value = (
+        mock_get_s3_cache.return_value = (
             {
                 "arn:aws:lambda:us-east-1:0:function:cloudwatch-event": [
                     "team:metrics",
@@ -659,10 +710,10 @@ class TestEnhancedLambdaMetrics(unittest.TestCase):
     @patch("enhanced_lambda_metrics.send_forwarder_internal_metrics")
     @patch("enhanced_lambda_metrics.get_cache_from_s3")
     def test_generate_enhanced_lambda_metrics_out_of_memory(
-        self, mock_build_cache, mock_forward_metrics
+        self, mock_get_s3_cache, mock_forward_metrics
     ):
 
-        mock_build_cache.return_value = (
+        mock_get_s3_cache.return_value = (
             {
                 "arn:aws:lambda:us-east-1:0:function:cloudwatch-event": [
                     "team:metrics",
