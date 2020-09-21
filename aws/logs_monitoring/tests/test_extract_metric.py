248a7561815dd25a9fed50d64e975d948be52f38
Sergio Prada <chechopradaster@gmail.com>
2020-09-21 10:24:32 -0400

    fix tests

--- aws/logs_monitoring/tests/test_extract_metric.py
+++ aws/logs_monitoring/tests/test_extract_metric.py
@@ -9,7 +9,10 @@ sys.modules["datadog_lambda.metric"] = MagicMock()
 sys.modules["datadog"] = MagicMock()
 sys.modules["requests"] = MagicMock()
 
-env_patch = patch.dict(os.environ, {"DD_API_KEY": "11111111111111111111111111111111"})
+env_patch = patch.dict(os.environ, {
+    "DD_API_KEY": "11111111111111111111111111111111",
+    "DD_ADDITIONAL_TARGET_LAMBDAS": "ironmaiden,megadeth",
+})
 env_patch.start()
 from lambda_function import extract_metric
 
