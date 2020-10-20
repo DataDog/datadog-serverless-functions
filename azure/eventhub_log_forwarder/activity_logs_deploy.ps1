97adc2e664754297f2dae2ec4f16db4131c36917
Claudia D'Adamo <claudia.dadamo@datadoghq.com>
2020-10-20 13:31:13 -0400

    [azure][automation] Add powershell deploy script for log forwarder (#368)
    * add powershell deploy script for log forwarder
    
    * cleanup
    
    * add more detail in comment
    
    * add changes
    
    * save progress
    
    * add site param
    
    * save status
    
    * fix var name
    
    * add additional logging
    
    * temp func template
    
    * change storage name temp
    
    * storage acct temp name
    
    * add temp name
    
    * final updates with new version
    
    * change variable name

---
+++ azure/eventhub_log_forwarder/activity_logs_deploy.ps1
@@ -0,0 +1,38 @@
+If (Test-Path variable:SubscriptionId) {} Else {Write-Error "`$SubscriptionId` must be set"}
+Set-AzContext -SubscriptionId $SubscriptionId
+
+$ResourceGroupName = If (Test-Path variable:ResourceGroupName) {$ResourceGroupName} Else {"datadog-log-forwarder-rg"}
+$ResourceGroupLocation = If (Test-Path variable:ResourceGroupLocation) {$ResourceGroupLocation} Else {"westus2"}
+$EventhubNamespace = If (Test-Path variable:EventhubNamespace) {$EventhubNamespace} Else {"datadog-eventhub-namespace"}
+$EventhubName = If (Test-Path variable:EventhubName) {$EventhubName} Else {"datadog-eventhub"}
+$FunctionAppName = If (Test-Path variable:FunctionAppName) {$FunctionAppName} Else {"datadog-functionapp"}
+$FunctionName = If (Test-Path variable:FunctionName) {$FunctionName} Else {"datadog-function"}
+$DiagnosticSettingName = If (Test-Path variable:DiagnosticSettingName) {$DiagnosticSettingName} Else {"datadog-activity-logs-diagnostic-setting"}
+$DatadogSite = If (Test-Path variable:DatadogSite) {$DatadogSite} Else {"datadoghq.com"}
+
+$code = (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/activity_logs_monitoring/index.js")
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+
+New-AzResourceGroupDeployment `
+    -TemplateUri "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/eventhub_log_forwarder/parent_template.json" `
+    -ResourceGroupName $ResourceGroupName `
+    -functionCode $code `
+    -apiKey $ApiKey `
+    -location $ResourceGroupLocation `
+    -eventhubNamespace $EventhubNamespace `
+    -eventHubName $EventhubName `
+    -functionAppName $FunctionAppName `
+    -functionName $FunctionName `
+    -datadogSite $DatadogSite `
+    -Verbose
+
+New-AzDeployment `
+    -TemplateUri "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/eventhub_log_forwarder/activity_log_diagnostic_settings.json" `
+    -eventHubNamespace $EventhubNamespace `
+    -eventHubName $EventhubName `
+    -settingName $DiagnosticSettingName `
+    -resourceGroup $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+    -Verbose
+
