8e84d6c63f77b66c6a235b86873fc2740a9a7e93
Claudia D'Adamo <claudia.dadamo@datadoghq.com>
2020-10-21 17:26:55 -0400

    add script for infra-only deployment

--- azure/eventhub_log_forwarder/resource_deploy.ps1
+++ azure/eventhub_log_forwarder/resource_deploy.ps1
@@ -1,22 +1,21 @@
-
-{
-If (Test-Path variable:SubscriptionId) {} Else {Write-Error "`$SubscriptionId` must be set"}
+&{
+If (Test-Path variable:SubscriptionId) {} Else {Write-Error "`$SubscriptionId` must be set"; Return }
+If (Test-Path variable:ApiKey) {} Else {Write-Error "`$ApiKey` must be set"; Return }
 Set-AzContext -SubscriptionId $SubscriptionId
-}
+
 $ResourceGroupLocation = If (Test-Path variable:ResourceGroupLocation) {$ResourceGroupLocation} Else {"westus2"}
-$ResourceGroupName = If (Test-Path variable:ResourceGroupName) {$ResourceGroupName} Else {"datadog-log-forwarder-rg" + $ResourceGroupLocation}
-$EventhubNamespace = If (Test-Path variable:EventhubNamespace) {$EventhubNamespace} Else {"datadog-eventhub-namespace" + $ResourceGroupLocation}
-$EventhubName = If (Test-Path variable:EventhubName) {$EventhubName} Else {"datadog-eventhub" + $ResourceGroupLocation}
-$FunctionAppName = If (Test-Path variable:FunctionAppName) {$FunctionAppName} Else {"datadog-functionapp" + $ResourceGroupLocation}
-$FunctionName = If (Test-Path variable:FunctionName) {$FunctionName} Else {"datadog-function" + $ResourceGroupLocation}
+$ResourceGroupName = If (Test-Path variable:ResourceGroupName) {$ResourceGroupName} Else {"datadog-log-forwarder-rg-" + $ResourceGroupLocation}
+$EventhubNamespace = If (Test-Path variable:EventhubNamespace) {$EventhubNamespace} Else {"datadog-eventhub-namespace-" + $ResourceGroupLocation}
+$EventhubName = If (Test-Path variable:EventhubName) {$EventhubName} Else {"datadog-eventhub-" + $ResourceGroupLocation}
+$FunctionAppName = If (Test-Path variable:FunctionAppName) {$FunctionAppName} Else {"datadog-functionapp-" + $ResourceGroupLocation}
+$FunctionName = If (Test-Path variable:FunctionName) {$FunctionName} Else {"datadog-function-" + $ResourceGroupLocation}
 $DatadogSite = If (Test-Path variable:DatadogSite) {$DatadogSite} Else {"datadoghq.com"}
 
 $code = (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/activity_logs_monitoring/index.js")
 
-If ($ResourceGroupName == "datadog-log-forwarder-rg" + $ResourceGroupLocation) {
-    New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
-}
+New-AzResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
 
+try {
 New-AzResourceGroupDeployment `
     -TemplateUri "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/eventhub_log_forwarder/parent_template.json" `
     -ResourceGroupName $ResourceGroupName `
@@ -28,4 +27,10 @@ New-AzResourceGroupDeployment `
     -functionAppName $FunctionAppName `
     -functionName $FunctionName `
     -datadogSite $DatadogSite `
-    -Verbose
+    -Verbose `
+    -ErrorAction Stop
+} catch {
+    Write-Error $_
+    Return
+}
+}
