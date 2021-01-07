843c285f50af5976dca4aec3da5b4868322e2a0f
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2021-01-06 22:25:31 -0500

    Add integration test

--- aws/logs_monitoring/tools/integration_tests/tester/test_snapshots.py
+++ aws/logs_monitoring/tools/integration_tests/tester/test_snapshots.py
@@ -140,3 +140,8 @@ class TestForwarderSnapshots(unittest.TestCase):
         input_filename = f"{snapshot_dir}/cloudwatch_log_timeout.json"
         snapshot_filename = f"{input_filename}~snapshot"
         self.compare_snapshot(input_filename, snapshot_filename)
+
+    def test_cloudwatch_log_legacy_metric(self):
+        input_filename = f"{snapshot_dir}/cloudwatch_log_legacy_metric.json"
+        snapshot_filename = f"{input_filename}~snapshot"
+        self.compare_snapshot(input_filename, snapshot_filename)
