3d061da905ffaa50acecce06f7f6f0c5dde3acf7
Tian Chu <tian.chu@datadoghq.com>
2021-02-26 12:03:28 -0500

    Bump version from 3.28.4 to 3.28.5

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -219,7 +219,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "3.28.4"
+DD_FORWARDER_VERSION = "3.28.5"
 
 # Additional target lambda invoked async with event data
 DD_ADDITIONAL_TARGET_LAMBDAS = get_env_var("DD_ADDITIONAL_TARGET_LAMBDAS", default=None)
