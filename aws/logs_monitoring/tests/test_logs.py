abda8e47a9a7cf45828c17a56df0e389784e69c7
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-03-23 17:00:38 -0400

    Fix patching

--- aws/logs_monitoring/tests/test_logs.py
+++ aws/logs_monitoring/tests/test_logs.py
@@ -1,6 +1,6 @@
 import unittest
-import os
 from unittest.mock import patch
+
 
 class TestFilterLogs(unittest.TestCase):
     example_logs = [
@@ -16,53 +16,63 @@ class TestFilterLogs(unittest.TestCase):
             "This is a 200",
         ]
 
-        with patch.dict(os.environ, {
-            "INCLUDE_AT_MATCH": r"\b[4|5][0-9][0-9]\b",
-        }):
+        with patch.multiple("settings", INCLUDE_AT_MATCH=r"\b[4|5][0-9][0-9]\b"):
             from logs import filter_logs
+
             filtered_logs = filter_logs(http_logs)
 
-        self.assertEqual(filtered_logs, [
-            "This is a 500",
-        ])
+        self.assertEqual(
+            filtered_logs,
+            [
+                "This is a 500",
+            ],
+        )
 
     def test_include_at_match(self):
-        with patch.dict(os.environ, {
-            "INCLUDE_AT_MATCH": r"^(START|END)",
-        }):
+        with patch.multiple("settings", INCLUDE_AT_MATCH=r"^(START|END)"):
             from logs import filter_logs
+
             filtered_logs = filter_logs(self.example_logs)
 
-        self.assertEqual(filtered_logs, [
-            "START RequestId: ...",
-            "END RequestId: ...",
-        ])
+        self.assertEqual(
+            filtered_logs,
+            [
+                "START RequestId: ...",
+                "END RequestId: ...",
+            ],
+        )
 
     def test_exclude_at_match(self):
-        with patch.dict(os.environ, {
-            "EXCLUDE_AT_MATCH": r"^(START|END)",
-        }):
+        with patch.multiple("settings", EXCLUDE_AT_MATCH=r"^(START|END)"):
             from logs import filter_logs
+
             filtered_logs = filter_logs(self.example_logs)
 
-        self.assertEqual(filtered_logs, [
-            "This is not a REPORT log",
-            "REPORT RequestId: ...",
-        ])
+        self.assertEqual(
+            filtered_logs,
+            [
+                "This is not a REPORT log",
+                "REPORT RequestId: ...",
+            ],
+        )
 
     def test_exclude_overrides_include(self):
-        with patch.dict(os.environ, {
-            "EXCLUDE_AT_MATCH": r"^END",
-            "INCLUDE_AT_MATCH": r"^(START|END)",
-        }):
+        with patch.multiple(
+            "settings", EXCLUDE_AT_MATCH=r"^END", INCLUDE_AT_MATCH=r"^(START|END)"
+        ):
             from logs import filter_logs
+
             filtered_logs = filter_logs(self.example_logs)
 
-        self.assertEqual(filtered_logs, [
-            "START RequestId: ...",
-        ])
+        self.assertEqual(
+            filtered_logs,
+            [
+                "START RequestId: ...",
+            ],
+        )
 
     def test_no_filtering_rules(self):
         from logs import filter_logs
+
         filtered_logs = filter_logs(self.example_logs)
         self.assertEqual(filtered_logs, self.example_logs)
