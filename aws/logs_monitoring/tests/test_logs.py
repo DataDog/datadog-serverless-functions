c920a081dc3b33eee6ead96ebb308522266783b6
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-03-23 17:06:33 -0400

    Try different patch

--- aws/logs_monitoring/tests/test_logs.py
+++ aws/logs_monitoring/tests/test_logs.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-
+import settings
 class TestFilterLogs(unittest.TestCase):
     example_logs = [
         "START RequestId: ...",
@@ -16,7 +16,7 @@ class TestFilterLogs(unittest.TestCase):
             "This is a 200",
         ]
 
-        with patch.multiple("settings", INCLUDE_AT_MATCH=r"\b[4|5][0-9][0-9]\b"):
+        with patch.multiple(settings, INCLUDE_AT_MATCH=r"\b[4|5][0-9][0-9]\b"):
             from logs import filter_logs
 
             filtered_logs = filter_logs(http_logs)
@@ -29,7 +29,7 @@ class TestFilterLogs(unittest.TestCase):
         )
 
     def test_include_at_match(self):
-        with patch.multiple("settings", INCLUDE_AT_MATCH=r"^(START|END)"):
+        with patch.multiple(settings, INCLUDE_AT_MATCH=r"^(START|END)"):
             from logs import filter_logs
 
             filtered_logs = filter_logs(self.example_logs)
@@ -43,7 +43,7 @@ class TestFilterLogs(unittest.TestCase):
         )
 
     def test_exclude_at_match(self):
-        with patch.multiple("settings", EXCLUDE_AT_MATCH=r"^(START|END)"):
+        with patch.multiple(settings, EXCLUDE_AT_MATCH=r"^(START|END)"):
             from logs import filter_logs
 
             filtered_logs = filter_logs(self.example_logs)
@@ -58,7 +58,7 @@ class TestFilterLogs(unittest.TestCase):
 
     def test_exclude_overrides_include(self):
         with patch.multiple(
-            "settings", EXCLUDE_AT_MATCH=r"^END", INCLUDE_AT_MATCH=r"^(START|END)"
+            settings, EXCLUDE_AT_MATCH=r"^END", INCLUDE_AT_MATCH=r"^(START|END)"
         ):
             from logs import filter_logs
 
