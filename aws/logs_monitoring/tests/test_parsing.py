1830eaef228a3b3244e8320d5e8a6efac425aa86
maxday <maxime.david@datadoghq.com>
2021-07-15 10:53:02 -0400

    add tests

--- aws/logs_monitoring/tests/test_parsing.py
+++ aws/logs_monitoring/tests/test_parsing.py
@@ -177,6 +177,15 @@ class TestParseEventSource(unittest.TestCase):
             "route53",
         )
 
+    def test_vpcdnsquerylogs_event(self):
+        self.assertEqual(
+            parse_event_source(
+                {"Records": ["logs-from-s3"]},
+                "AWSLogs/123456779121/vpcdnsquerylogs/vpc-********/2021/05/11/vpc-********_vpcdnsquerylogs_********_20210511T0910Z_71584702.log.gz",
+            ),
+            "route53",
+        )
+
     def test_fargate_event(self):
         self.assertEqual(
             parse_event_source(
