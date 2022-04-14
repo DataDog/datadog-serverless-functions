10aa885807bb6d37861f71d294cfde4cd8e21103
Harvinder <ghotra.harvinder@gmail.com>
2022-04-14 19:53:11 -0400

    Lint

--- aws/logs_monitoring/tools/integration_tests/recorder/pb/trace_payload_pb2.py
+++ aws/logs_monitoring/tools/integration_tests/recorder/pb/trace_payload_pb2.py
@@ -7,26 +7,34 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
+
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
+import trace_pb2 as trace__pb2
+import span_pb2 as span__pb2
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13trace_payload.proto\x12\x02pb\x1a\x0btrace.proto\x1a\nspan.proto\"K\n\x0cTracePayload\x12\x10\n\x08hostName\x18\x01 \x01(\t\x12\x0b\n\x03\x65nv\x18\x02 \x01(\t\x12\x1c\n\x06traces\x18\x03 \x03(\x0b\x32\x0c.pb.APITraceb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
+    b'\n\x13trace_payload.proto\x12\x02pb\x1a\x0btrace.proto\x1a\nspan.proto"K\n\x0cTracePayload\x12\x10\n\x08hostName\x18\x01 \x01(\t\x12\x0b\n\x03\x65nv\x18\x02 \x01(\t\x12\x1c\n\x06traces\x18\x03 \x03(\x0b\x32\x0c.pb.APITraceb\x06proto3'
+)
 
 
-
-_TRACEPAYLOAD = DESCRIPTOR.message_types_by_name['TracePayload']
-TracePayload = _reflection.GeneratedProtocolMessageType('TracePayload', (_message.Message,), {
-  'DESCRIPTOR' : _TRACEPAYLOAD,
-  '__module__' : 'trace_payload_pb2'
-  # @@protoc_insertion_point(class_scope:pb.TracePayload)
-  })
+_TRACEPAYLOAD = DESCRIPTOR.message_types_by_name["TracePayload"]
+TracePayload = _reflection.GeneratedProtocolMessageType(
+    "TracePayload",
+    (_message.Message,),
+    {
+        "DESCRIPTOR": _TRACEPAYLOAD,
+        "__module__": "trace_payload_pb2"
+        # @@protoc_insertion_point(class_scope:pb.TracePayload)
+    },
+)
 _sym_db.RegisterMessage(TracePayload)
 
 if _descriptor._USE_C_DESCRIPTORS == False:
 
-  DESCRIPTOR._options = None
-  _TRACEPAYLOAD._serialized_start=52
-  _TRACEPAYLOAD._serialized_end=127
+    DESCRIPTOR._options = None
+    _TRACEPAYLOAD._serialized_start = 52
+    _TRACEPAYLOAD._serialized_end = 127
 # @@protoc_insertion_point(module_scope)
