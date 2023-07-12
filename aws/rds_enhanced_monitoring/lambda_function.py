6153f07b7d3e59f54b085929bd7eedcbe6804bfe
Raphael Allier <raphael.allier@datadoghq.com>
2023-07-12 16:56:28 +0200

    [AWSC] Adding specific exceptions

--- aws/rds_enhanced_monitoring/lambda_function.py
+++ aws/rds_enhanced_monitoring/lambda_function.py
@@ -70,12 +70,18 @@ def _datadog_keys():
             # need to re-encode this in base64
             try:
                 DD_API_KEY = DD_API_KEY.decode("utf-8")
-            except:
+            except UnicodeDecodeError as e:
                 print(
-                    "INFO DD_KMS_API_KEY: Could not decode key in utf-8, encoding in b64"
+                    "INFO DD_KMS_API_KEY: Could not decode key in utf-8, encoding in b64. Exception:",
+                    e
                 )
                 DD_API_KEY = base64.b64encode(DD_API_KEY)
                 DD_API_KEY = DD_API_KEY.decode("utf-8")
+            except Exception as e:
+                print(
+                    "ERROR DD_KMS_API_KEY Unknown exception decoding key:",
+                    e
+                )
         return {"api_key": DD_API_KEY}
 
     if "DD_API_KEY" in os.environ:
