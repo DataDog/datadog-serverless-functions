4380bca8ca2efbbd685139cdf3e65b52f03df677
Michel Daviot <michel.daviot@datadoghq.com>
2023-12-06 11:40:15 +0100

    fix approval tests in CI

--- aws/logs_monitoring/tests/__init__.py
+++ aws/logs_monitoring/tests/__init__.py
@@ -1,13 +1,17 @@
 from approvaltests.approvals import set_default_reporter
 from approvaltests.reporters import GenericDiffReporter, GenericDiffReporterConfig
+from os import environ
+from os.path import exists
 
-
-set_default_reporter(
-    GenericDiffReporter(
-        GenericDiffReporterConfig(
-            "VSCODE",
-            "/Applications/Visual Studio Code.app/contents/Resources/app/bin/code",
-            ["-d"],
+# TODO: this can be removed when a new verison of approvaltests is released 
+# with this fix: https://github.com/approvals/ApprovalTests.Python/pull/150
+if not environ.get("CI") and exists("/Applications/Visual Studio Code.app/contents/Resources/app/bin/code"): 
+    set_default_reporter(
+        GenericDiffReporter(
+            GenericDiffReporterConfig(
+                "VSCODE",
+                "/Applications/Visual Studio Code.app/contents/Resources/app/bin/code",
+                ["-d"],
+            )
         )
     )
-)
