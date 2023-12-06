a1d8342b9b8a43795a83939228cf46c7337bb439
Michel Daviot <michel.daviot@datadoghq.com>
2023-12-06 11:47:13 +0100

    black

--- aws/logs_monitoring/tests/__init__.py
+++ aws/logs_monitoring/tests/__init__.py
@@ -3,9 +3,11 @@ from approvaltests.reporters import GenericDiffReporter, GenericDiffReporterConf
 from os import environ
 from os.path import exists
 
-# TODO: this can be removed when a new verison of approvaltests is released 
+# TODO: this can be removed when a new verison of approvaltests is released
 # with this fix: https://github.com/approvals/ApprovalTests.Python/pull/150
-if not environ.get("CI") and exists("/Applications/Visual Studio Code.app/contents/Resources/app/bin/code"): 
+if not environ.get("CI") and exists(
+    "/Applications/Visual Studio Code.app/contents/Resources/app/bin/code"
+):
     set_default_reporter(
         GenericDiffReporter(
             GenericDiffReporterConfig(
