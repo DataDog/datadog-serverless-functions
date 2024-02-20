c193826a9ad36c86db2f9f115c75ff61a027ac6a
Ivan Klishch <ivan@klishch.com>
2024-02-20 16:13:56 -0500

    Bump version from 3.100.0 to 3.101.0

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -237,7 +237,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "3.100.0"
+DD_FORWARDER_VERSION = "3.101.0"
 
 # Additional target lambda invoked async with event data
 DD_ADDITIONAL_TARGET_LAMBDAS = get_env_var("DD_ADDITIONAL_TARGET_LAMBDAS", default=None)
