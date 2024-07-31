2f0555cae0385e1b8f27352c212f1a084ea5e3c3
Abhinav Vedmala <abhinav.vedmala@datadoghq.com>
2024-07-31 13:58:06 -0400

    Fixed template condition

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -59,9 +59,11 @@ DD_API_KEY = "<YOUR_DATADOG_API_KEY>"
 DD_FORWARD_LOG = get_env_var("DD_FORWARD_LOG", "true", boolean=True)
 
 ## @param DD_STEP_FUNCTION_TRACE_ENABLED - boolean - optional - default: true
-## Set this variable to `False` to disable log to trace conversion for Step Functions, while continuing to forward logs.
+## Set this variable to `True` to enable log to trace conversion for Step Functions.
 #
-DD_STEP_FUNCTION_TRACE_ENABLED = get_env_var("DD_STEP_FUNCTION_TRACE_ENABLED", default="true", boolean=True)
+DD_STEP_FUNCTION_TRACE_ENABLED = get_env_var(
+    "DD_STEP_FUNCTION_TRACE_ENABLED", default="false", boolean=True
+)
 
 ## @param DD_USE_TCP - boolean - optional -default: false
 ## Change this value to `true` to send your logs and metrics using the TCP network client
