6f4ac97fed75f2093071ab5f6ad54b77774fbf10
Katie McKew <katie.mckew@datadoghq.com>
2025-01-30 10:15:05 -0500

    ci(release): Update version from 4.0.1 to 4.0.2
    Signed-off-by: Katie McKew <katie.mckew@datadoghq.com>

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -267,7 +267,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "4.0.1"
+DD_FORWARDER_VERSION = "4.0.2"
 
 # CONST STRINGS
 AWS_STRING = "aws"
