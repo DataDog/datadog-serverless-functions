0fb58a93fa4203af99122a50e3c6bf92dff7afce
Vincent Boutour <vincent.boutour@datadoghq.com>
2025-05-16 18:08:07 +0200

    feat(logs): AWSX-1592 Move dms, docdb, fsx and opensearch to logs-backend
    Signed-off-by: Vincent Boutour <vincent.boutour@datadoghq.com>

--- aws/logs_monitoring/tools/integration_tests/tester/test_snapshots.py
+++ aws/logs_monitoring/tools/integration_tests/tester/test_snapshots.py
@@ -154,11 +154,6 @@ class TestForwarderSnapshots(unittest.TestCase):
         snapshot_filename = f"{input_filename}~snapshot"
         self.compare_snapshot(input_filename, snapshot_filename)
 
-    def test_cloudwatch_log_fsx_windows(self):
-        input_filename = f"{snapshot_dir}/cloudwatch_log_fsx_windows.json"
-        snapshot_filename = f"{input_filename}~snapshot"
-        self.compare_snapshot(input_filename, snapshot_filename)
-
     def test_cloudwatch_log_lambda_invocation(self):
         input_filename = f"{snapshot_dir}/cloudwatch_log_lambda_invocation.json"
         snapshot_filename = f"{input_filename}~snapshot"
