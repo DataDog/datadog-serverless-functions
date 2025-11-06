416c5089582c4fcae672e616289df981e225ce8f
Matt Spurlin <matt.spurlin@datadoghq.com>
2025-11-06 18:46:54 -0500

    Reapply "update event hub forwarder and template"
    This reverts commit 98b23bcdcc283e63039aa457404b994876e4d611.

--- azure/activity_logs_monitoring/index.js
+++ azure/activity_logs_monitoring/index.js
@@ -5,7 +5,7 @@
 
 const { app, InvocationContext } = require('@azure/functions');
 
-const VERSION = '2.1.1';
+const VERSION = '2.2.0';
 
 const STRING = 'string'; // example: 'some message'
 const STRING_ARRAY = 'string-array'; // example: ['one message', 'two message', ...]
@@ -29,7 +29,7 @@ const DD_SOURCE = process.env.DD_SOURCE || 'azure';
 const DD_SOURCE_CATEGORY = process.env.DD_SOURCE_CATEGORY || 'azure';
 const DD_PARSE_DEFENDER_LOGS = process.env.DD_PARSE_DEFENDER_LOGS; // Boolean whether to enable special parsing of Defender for Cloud logs. Set to 'false' to disable
 
-const EVENTHUB_NAME = process.env.EVENTHUB_NAME || 'datadog-eventhub'; // The event hub name inside your eventhub namespace
+const EVENTHUB_NAME = process.env.EVENTHUB_NAME || 'datadogEventhub'; // The event hub name inside your eventhub namespace
 
 const MAX_RETRIES = 4; // max number of times to retry a single http request
 const RETRY_INTERVAL = 250; // amount of time (milliseconds) to wait before retrying request, doubles after every retry
