b2c4734201ed6454f10220db268ff3ca6894eb50
Vincent Boutour <vincent.boutour@datadoghq.com>
2025-12-12 17:19:21 +0100

    ci(docker): Make apple container usable in place of docker
    Signed-off-by: Vincent Boutour <vincent.boutour@datadoghq.com>

--- aws/logs_monitoring/trace_forwarder/scripts/build_linux_go_bin.sh
+++ aws/logs_monitoring/trace_forwarder/scripts/build_linux_go_bin.sh
@@ -13,14 +13,27 @@ cd $(dirname "$0")/..
 
 rm -rf ./bin
 
+# Make us of Docker or apple/container seamless
+docker_build() {
+    docker buildx build "${@}"
+}
+
+if command -v container >/dev/null 2>&1; then
+    docker() {
+        container "${@}"
+    }
+
+    docker_build() {
+        container build "${@}"
+    }
+fi
+
 # Install datadogpy in a docker container to avoid the mess from switching
 # between different python runtimes.
 
 if [[ $(docker image ls | grep -c golang) -lt 1 ]]; then
-    docker buildx build --platform linux/arm64 -t golang . --no-cache --build-arg "runtime=python:3.13"
+    docker_build --platform linux/arm64 -t golang . --no-cache --build-arg "runtime=python:3.13"
 fi
 
-id=$(docker create --platform linux/arm64 golang)
-docker cp "${id}:/go/src/github.com/DataDog/datadog-serverless-functions/aws/logs_monitoring/trace_forwarder/bin" .
-docker rm -v "${id}"
+docker run --rm --platform linux/arm64 --volume "$(pwd):/root/bin" golang cp -r /go/src/github.com/DataDog/datadog-serverless-functions/aws/logs_monitoring/trace_forwarder/bin /root/bin
 echo "Done creating archive bin"
