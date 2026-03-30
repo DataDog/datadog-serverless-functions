fc5c2d4c518180d64620588ad4d234c0ad76d89f
Ivan Klishch <ivan.klishch@datadoghq.com>
2026-03-30 10:34:07 -0400

    ci(release): Update version from 5.3.0 to 5.4.0 (#1091)
    Signed-off-by: Ivan Klishch <ivan@klishch.com>

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -337,7 +337,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "5.3.0"
+DD_FORWARDER_VERSION = "5.4.0"
 
 # CONST STRINGS
 AWS_STRING = "aws"
