ecbeb5d1311d4c475c4e1f709a388835142a02db
Nabil Dakkoune <nabil.dakkoune@datadoghq.com>
2026-05-25 21:23:57 +0200

    add MaxIdleConnsPerHost custom

--- aws/logs_monitoring_go/internal/forwarding/client.go
+++ aws/logs_monitoring_go/internal/forwarding/client.go
@@ -24,6 +24,7 @@ var Client = newClient()
 func newClient() *http.Client {
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.TLSHandshakeTimeout = tlsHandshakeTimeout
+	transport.MaxIdleConnsPerHost = maxConcurrency
 	transport.DialContext = (&net.Dialer{
 		Timeout:   dialContextTimeout,
 		KeepAlive: dialContextKeepAlive,
