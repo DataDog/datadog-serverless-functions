580bf3b5341822cbfb455fd187c8e5e5b500c45a
Hari Krishnan Ramachandran <hari.ramachandran@datadoghq.com>
2026-07-10 15:39:13 -0400

    ci(release): Update version from 5.4.5 to 5.4.6
    Signed-off-by: Hari Krishnan Ramachandran <hari.ramachandran@datadoghq.com>

--- aws/logs_monitoring/settings.py
+++ aws/logs_monitoring/settings.py
@@ -344,7 +344,7 @@ DD_SOURCE = "ddsource"
 DD_CUSTOM_TAGS = "ddtags"
 DD_SERVICE = "service"
 DD_HOST = "host"
-DD_FORWARDER_VERSION = "5.4.5"
+DD_FORWARDER_VERSION = "5.4.6"
 
 # CONST STRINGS
 AWS_STRING = "aws"
